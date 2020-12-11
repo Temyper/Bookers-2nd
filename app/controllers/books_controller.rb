ca27770a0bd861051d5fdd65ea0f87680d715177
Temyper <pall47g1@gmail.com>
2020-12-11 16:04:34 +0000

    [Add] 1:Show, Edit, Destroy pages. 2:succeessfully

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,48 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+def create
+
+  book = Book.new(book_params)
+book.save
+flash[:success] = 'Book was successfully created.'
+redirect_to book_path(book.id)
+end
+
+def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  flash[:success] = 'Book was successfully destroyed.'
+  redirect_to books_path
+end
+
+def update
+book = Book.find(params[:id])
+book.update(book_params)
+flash[:success] = 'Book was successfully updated.'
+redirect_to book_path(book.id)
+end
 
   def top
 
+  end
+
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
