7b69f164979d73b02b5751e8fa01b650735065cf
Temyper <pall47g1@gmail.com>
2020-12-12 03:40:36 +0000

    [Leave] A unclear If-Syntax error.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,48 +1,85 @@
 class BooksController < ApplicationController
-  def index
+
+ def create
+
+    @book = Book.new(book_params)
+
+    @book.save
+
+    flash[:success] = 'Book was successfully created.'
+
+    redirect_to book_path(@book.id)
+# 20201212なぜかif文がエラーを起こす。エラー発生の最小単位は一番下
+    # if　@book.save
+
+    #   flash[:success] = 'Book was successfully created.'
+
+    #   redirect_to book_path(@book.id)
+
+    # else
+
+    #   render books_path
+
+    # end
+
+ end
+
+
+ def index
     @books = Book.all
     @book = Book.new
-  end
+ end
+
 
   def show
     @book = Book.find(params[:id])
   end
 
   def new
+    # @book = Book.new
   end
 
   def edit
     @book = Book.find(params[:id])
   end
 
-def create
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:success] = 'Book was successfully destroyed.'
+    redirect_to books_path
+  end
 
-  book = Book.new(book_params)
-book.save
-flash[:success] = 'Book was successfully created.'
-redirect_to book_path(book.id)
-end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:success] = 'Book was successfully updated.'
+    redirect_to book_path(book.id)
+  end
 
-def destroy
-  book = Book.find(params[:id])
-  book.destroy
-  flash[:success] = 'Book was successfully destroyed.'
-  redirect_to books_path
-end
-
-def update
-book = Book.find(params[:id])
-book.update(book_params)
-flash[:success] = 'Book was successfully updated.'
-redirect_to book_path(book.id)
-end
 
   def top
 
   end
 
-   private
+
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
+
+# if文エラーの最小単位
+# class BooksController < ApplicationController
+
+# def create
+
+#     if　1+1==2
+
+#     end
+
+# end
+
+# end
